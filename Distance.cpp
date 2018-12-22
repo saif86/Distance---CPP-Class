e0cc418f5d6f4e39657a1eccd170a5700de7d4b7
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2018-12-22 20:04:13 +0800

    Update Distance.cpp

--- Distance.cpp
+++ Distance.cpp
@@ -1,55 +1,147 @@
-#include<iostream> 
+/**
+* @file Distance.cpp
+*
+* @brief C++ Program to demostrate Distance class for following tasks.
+*
+* TASK 1: Class definition with data members for feet and inches. Defining member functions for setting and displaying the distance.
+* TASK 2: Defining default and overloaded constructor.
+* TASK 3: Updating the set distance so that inches never exceeds 11
+* TASK 4: Defining a member function for adding a distance to the currrent object.
+*
+* @author Saif Ullah Ijaz
+*
+*/
 
-using std::cout; 
-using std::endl; 
-using std::cin; 
+// SYSTEM INCLUDES
+#include <iostream> 
+using namespace std;
 
-class Distance{ 
-	int Feet; 
-	float Inches; 
-public: 
-	Distance(int, float); 
-	void Set_distance(int, float); /*assign values to the class attributes*/
-	void Display_distance();	/*display the values of class attributes*/
-	void Add_distance(Distance); 
-	int getFeet(); 
-	float getInches(); 
-}; 
+// Distance class definition
+class Distance {
+public:
+// LIFECYCLE
+	/** Default + Overloaded constructor.
+	*/
+	Distance(int = 0, float = 0.0);
 
-Distance::Distance(int aFeet = 0, float aInches = 0.0){ 
-	Set_distance(aFeet, aInches); 
-} 
+// OPERATIONS
+	/** function that displays the distance.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void DisplayDistance();
 
-void Distance::Set_distance(int aFeet, float aInches){ 
-	Feet = aFeet; 
-	if (aInches > 11){ 
-		Feet += int(aInches) / 12; 
-		Inches = int(aInches) % 12; 
-	} 
-	else{ 
-		Inches = aInches; 
-	} 
-} 
+	/** function that adds another distance object to the current distance.
+	*
+	* @param aDistance The reference to a constant distance object to be added
+	*
+	* @return void
+	*/
+	void AddDistance(const Distance& aDistance);
 
-void Distance::Display_distance(){ 
-	cout << "The distance is: " << Feet << " Feet and " << Inches << " Inches." << endl; 
-} 
+// ACCESS
+	// setters
+	void SetFeet(int = 0);
+	void SetInches(float = 0.0);
+	void SetDistance(int = 0, float = 0.0);
+	/**
+	# @overload void SetDistance(const Distance& aDistance);
+	*/
+	void SetDistance(const Distance& aDistance);
 
-int Distance::getFeet(){ 
-	return Feet; 
-} 
+	// getters
+	int GetFeet() const;
+	float GetInches() const;
+	const Distance& GetDistance()const;
 
-float Distance::getInches(){ 
-	return Inches; 
-} 
+private:
 
-void Distance::Add_distance(Distance d){ 
-	int tFeet,tInches; 
-	tFeet= Feet + d.getFeet(); 
-	tInches = Inches + d.getInches();
-	Set_distance(tFeet, tInches); 
-} 
+// DATA MEMBERS
+	int mFeet;
+	float mInches;
+};
+// end class Distance
 
+// File scope starts here 
+
+/////////////////////////////// PUBLIC ///////////////////////////////////////
+
+//============================= LIFECYCLE ====================================
+
+// Distance Default+Overloaded Constructor
+Distance::Distance(int aFeet, float aInches) : mFeet(aFeet), mInches(aInches) {
+	this->SetDistance(aFeet, aInches);
+}
+// end Distance constructor 
+
+//============================= OPERATIONS ===================================
+// function that displays the distance.
+void Distance::DisplayDistance() {
+	cout << "The distance is: " << this->GetFeet() << " Feet and " << this->GetInches() << " Inches." << endl;
+}
+// end function DisplayDistance
+
+// function that adds another distance object to the current distance.
+void Distance::AddDistance(const Distance& aDistance) {
+	int tFeet;
+	float tInches;
+	tFeet = this->GetFeet() + aDistance.GetFeet();
+	tInches = this->GetInches() + aDistance.GetInches();
+	this->SetDistance(tFeet, tInches);
+}
+// end function AddDistance
+
+//============================= ACESS      ===================================
+
+// function that sets Feet
+void Distance::SetFeet(int aFeet) {
+	this->mFeet = aFeet;
+}
+// end function SetFeet
+
+// function that sets Inches
+void Distance::SetInches(float aInches) {
+	this->mInches = int(aInches) % 12;
+	this->mFeet += aInches / 12;
+}
+// end function SetInches
+
+// function that sets Distance
+void Distance::SetDistance(int aFeet, float aInches) {
+	this->SetFeet(aFeet);
+	this->SetInches(aInches);
+}
+// end function SetDistance
+
+// function that sets the Distance
+void Distance::SetDistance(const Distance &obj) {
+	this->SetDistance(obj.GetFeet(), obj.GetInches());
+}
+// end function SetDistance
+
+// function that gets Feet
+int Distance::GetFeet() const {
+	return this->mFeet;
+}
+// end function GetFeet
+
+// function that gets Inches
+float Distance::GetInches() const {
+	return this->mInches;
+}
+// end function GetInches
+
+// function that gets the Distance
+const Distance& Distance::GetDistance()const {
+	return *this;
+}
+// end function GetDistance
+
+/////////////////////////////// MAIN   ///////////////////////////////////////
+
+// function main begins program execution
 void main() {
 	{
 		cout << "################ TASK 1 ######################" << endl;
@@ -60,35 +152,36 @@ void main() {
 		cin >> dFeet;
 		cout << "Enter distance in Inches: ";
 		cin >> dInches;
-		d0.Set_distance(dFeet, dInches);  // assigns values to the class attributes
+		d0.SetDistance(dFeet, dInches);  // assigns values to the class attributes
 
-		d0.Display_distance(); // displays the values of class attributes
+		d0.DisplayDistance(); // displays the values of class attributes
 		cout << "################ END OF TASK 1 ################\n" << endl;
 	}
 
 	{
 		cout << "################ TASK 2 ######################" << endl;
 		Distance d1, d2(10, 2); // initializes the values of d1 to 0 and values of d2 to 10, 2
-		d1.Display_distance();
-		d2.Display_distance();
+		d1.DisplayDistance();
+		d2.DisplayDistance();
 		cout << "################ END OF TASK 2 ################\n" << endl;
 	}
 
 	{
 		cout << "################ TASK 3 ######################" << endl;
 		Distance obj(10, 12);	// the value of obj members will be feet: 11, inches:0
-		obj.Display_distance();
-		obj.Set_distance(4, 14); //the value of obj member will be feet: 5, inches:2
-		obj.Display_distance();
+		obj.DisplayDistance();
+		obj.SetDistance(4, 14); //the value of obj member will be feet: 5, inches:2
+		obj.DisplayDistance();
 		cout << "################ END OF TASK 3 ################\n" << endl;
 	}
 
 	{
 		cout << "################ TASK 4 ######################" << endl;
 		Distance d1(0, 3), d2(1, 2);
-		d1.Add_distance(d2); // the function should add the value of d2 to d1
-		d1.Display_distance(); // the values printed should be 1,5
+		d1.AddDistance(d2); // the function should add the value of d2 to d1
+		d1.DisplayDistance(); // the values printed should be 1,5
 		cout << "################ END OF TASK 4 ################\n" << endl;
 	}
-	system("pause"); 
+	system("pause");
 }
+// end main 
